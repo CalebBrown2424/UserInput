1f4c7fef0eaea5a74be44e834d3fdf6ddda22318
Caleb Brown <56742987+CalebBrown2424@users.noreply.github.com>
2020-08-27 13:12:03 -0700

    Added more features, user input, second activity page(warning: it is faulty) next activity button

--- app/src/main/java/com/example/userinput/MainActivity.java
+++ app/src/main/java/com/example/userinput/MainActivity.java
@@ -6,22 +6,31 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
-
+EditText editText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         configureNextButton();
+        editText = (EditText) findViewById(R.id.editTextTextPersonName);
+
     }
+
         private void configureNextButton()
         {
         ImageButton button = (ImageButton) findViewById(R.id.imageButton1);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
+                String data = editText.getText().toString();
+                Intent intent = new Intent(MainActivity.this, MainActivity2.class);
+                intent.putExtra("data", data);
+                startActivity(intent);
                 startActivity(new Intent(MainActivity.this, MainActivity2.class));
             }
         });
--- app/src/main/java/com/example/userinput/MainActivity2.java
+++ app/src/main/java/com/example/userinput/MainActivity2.java
@@ -2,18 +2,49 @@ package com.example.userinput;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.TextView;
 
-public class MainActivity2 extends AppCompatActivity {
-
+public class MainActivity2 extends AppCompatActivity   {
+    TextView textElement;
+    Bundle bundle;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
         configureNextButton();
+        changeText();
+
+//String value2;
+
+
+    }
+
+    public void changeText()
+    {
+        TextView textElement1 = (TextView) findViewById(R.id.textView4);
+        bundle = getIntent().getExtras();
+
+        if(bundle.getString("data") == null)
+        {
+            finish();
+        }
+        
+            String value2 = bundle.getString("data");
+
+        if(value2.contains(" "))
+        {
+            String value3 = value2.replaceAll(" ", "");
+            textElement1.setText("Hello " + value3);
+        }
+        else {
+            textElement1.setText("Hello " + value2);
+
+        };
     }
     private void configureNextButton()
     {
